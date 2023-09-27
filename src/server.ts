09cc96f7e1405d15e2633f44eb46e594bfc8193e
Allan Almeida <almeidaa405@gmail.com>
2023-09-27 09:26:36 -0400

    undo change server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,20 @@
-import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
 import { z } from 'zod';
-import fastifyCors from 'fastify-cors';
-import fastifyStatic from 'fastify-static';
-import path from 'path';
+import cors from '@fastify/cors';
 
 const app = fastify();
-
-// Registrar o plugin de CORS
-app.register(fastifyCors, {
+app.register(cors, {
   origin: '*',
 });
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-// Configurar a pasta para servir arquivos estáticos
-app.register(fastifyStatic, {
-  root: path.join(__dirname, '/src'), // Substitua 'public' pelo nome da pasta onde estão seus arquivos estáticos
-  wildcard: false, // Defina como false se você não quiser que arquivos não encontrados disparem uma resposta 404
-});
-
-// Rota para a página inicial (index.html)
-app.get('/', (request, reply) => {
-  // O arquivo 'index.html' será servido automaticamente pela rota raiz devido à configuração do fastify-static.
-  // Não é necessário especificar sendFile aqui.
-  reply.send();
-});
-
-// Suas rotas existentes para o backend
-app.get('/users', async (request, reply) => {
+app.get('/users', async () => {
   const users = await prisma.user.findMany();
+
   return { users };
-});
+})
 
 app.post('/users', async (request, reply) => {
   const createUsersSchema = z.object({
@@ -40,16 +23,16 @@ app.post('/users', async (request, reply) => {
     whatsapp: z.string(),
     expectations: z.string(),
     availability: z.string(),
-    discovery: z.string(),
+    discovery: z.string()
   });
-
-  const {
+  
+  const { 
     name,
     email,
     whatsapp,
     expectations,
     availability,
-    discovery,
+    discovery 
   } = createUsersSchema.parse(request.body);
 
   await prisma.user.create({
@@ -59,19 +42,16 @@ app.post('/users', async (request, reply) => {
       whatsapp,
       expectations,
       availability,
-      discovery,
-    },
+      discovery
+    }
   });
 
   return reply.status(201).send();
-});
+})
 
-const PORT = process.env.PORT || 3333;
-
-app.listen(PORT, '0.0.0.0', (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`HTTP Server Running on Port ${PORT}`);
-});
+app.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333,
+}).then(() => {
+  console.log('HTTP Server Running')
+})
