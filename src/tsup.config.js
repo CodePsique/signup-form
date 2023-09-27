400c84ecf610c5122ebb1c617a9029012efc8a66
Allan Almeida <almeidaa405@gmail.com>
2023-09-26 23:11:55 -0400

    uninstall html loader

--- src/tsup.config.js
+++
@@ -1,9 +0,0 @@
-// tsup.config.js
-import { defineConfig } from 'tsup';
-import html from 'esbuild-plugin-html';
-
-export default defineConfig({
-  // ... outras configurações
-  plugins: [html()],
-});
-
