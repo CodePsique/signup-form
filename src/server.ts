b287bfa118fbe1457f561129c9f1d477cdcddaac
Allan Almeida <almeidaa405@gmail.com>
2023-09-27 16:20:46 -0400

    Trying to make the html appear again

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,42 @@
-import fastify from 'fastify';
-import fastifyStatic from 'fastify-static';
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+import { z } from 'zod';
+import cors from '@fastify/cors';
 import * as fs from 'fs';
 import * as path from 'path';
-import { PrismaClient } from '@prisma/client';
-import { z } from 'zod';
+
 
 const app = fastify();
+app.register(cors, {
+  origin: '*',
+});
 
-// Configurar a pasta de arquivos estáticos
-const srcFolderPath = path.join(__dirname, 'src');
+const publicFolderPath = path.join(__dirname, 'src');
 
-// Rota para servir arquivos HTML
 app.get('/', (request, reply) => {
-  const indexPath = path.join(srcFolderPath, 'index.html');
-
+  const indexPath = path.join(publicFolderPath, 'index.html');
+  
+  // Leia o conteúdo do arquivo
   fs.readFile(indexPath, 'utf-8', (err, fileContent) => {
     if (err) {
+      // Se ocorrer um erro ao ler o arquivo, envie uma resposta de erro
       reply.status(500).send('Internal Server Error');
       return;
     }
+
+    // Envie o conteúdo do arquivo HTML como resposta
     reply.type('text/html').send(fileContent);
   });
 });
 
-// Rota para servir arquivos estáticos (CSS, imagens, scripts, etc.)
-app.register(fastifyStatic, {
-  root: srcFolderPath, // Diretório de arquivos estáticos
-});
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 app.get('/users', async () => {
   const users = await prisma.user.findMany();
 
   return { users };
-});
+})
 
 app.post('/users', async (request, reply) => {
   const createUsersSchema = z.object({
@@ -43,16 +45,16 @@ app.post('/users', async (request, reply) => {
     whatsapp: z.string(),
     expectations: z.string(),
     availability: z.string(),
-    discovery: z.string(),
+    discovery: z.string()
   });
-
-  const {
+  
+  const { 
     name,
     email,
     whatsapp,
     expectations,
     availability,
-    discovery,
+    discovery 
   } = createUsersSchema.parse(request.body);
 
   await prisma.user.create({
@@ -62,16 +64,16 @@ app.post('/users', async (request, reply) => {
       whatsapp,
       expectations,
       availability,
-      discovery,
-    },
+      discovery
+    }
   });
 
   return reply.status(201).send();
-});
+})
 
 app.listen({
   host: '0.0.0.0',
   port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }).then(() => {
-  console.log('HTTP Server Running');
-});
+  console.log('HTTP Server Running')
+})
